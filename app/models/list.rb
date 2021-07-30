671950fdd364248136f4f9f2e7f75acbf2e097f0
carolina fuentes <fuecarolina@gmail.com>
2021-07-30 12:38:04 -0400

    list

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,5 @@ has_many :bookmarks, dependent: :destroy
 has_many :movies, through: :bookmarks
 
 validates :name, presence: true, uniqueness: true
-
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lists do
-    resources :movies
+    resources :bookmarks
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
